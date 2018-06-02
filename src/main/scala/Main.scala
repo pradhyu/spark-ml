028606f5fac081dae55a98f71c5b9520378447b0
PK <pradhyu.shrestha@gmail.com>
2018-06-02 11:05:08 -0400

    ensime spark ml

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,9 @@
+object Main extends App {
+  println("Hello, World!")
+
+  def square(x: Int): Int ={
+    x*x
+  }
+  square(100)
+}
+ 
---
+++ src/main/scala/SparmMeApp.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.{SparkContext, SparkConf}
+
+object SparkMeApp {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("SparkMe Application")
+    val sc = new SparkContext(conf)
+
+    val fileName = args(0)
+    val lines = sc.textFile(fileName).cache
+
+    val c = lines.count
+    println(s"There are $c lines in $fileName")
+  }
+}
